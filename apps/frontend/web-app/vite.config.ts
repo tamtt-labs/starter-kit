63c2caabc782226307d0cd276b3879f9621ce874
Tam Tran <tamtt@dgroup.co>
2026-03-15 11:51:04 +0700

    fix: audit type

--- apps/frontend/web-app/vite.config.ts
+++ apps/frontend/web-app/vite.config.ts
@@ -1,8 +1,8 @@
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -10,10 +10,7 @@ export default defineConfig({
     viteTsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
+    viteReact(),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 });

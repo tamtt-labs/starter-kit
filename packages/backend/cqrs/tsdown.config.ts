968adb6f054e85d64acac9130cca87ffa8d91f7f
Trần Thanh Tâm <tamtt@Tams-MacBook-Pro.local>
2026-04-29 17:59:23 +0700

    feat(api-gateway): dockerize

--- packages/backend/cqrs/tsdown.config.ts
+++ packages/backend/cqrs/tsdown.config.ts
@@ -2,13 +2,10 @@ import { base } from "@tamtt-labs/tsdown";
 import { defineConfig, mergeConfig } from "tsdown";
 
 export default defineConfig((inlineConfig, context) =>
-  mergeConfig(
-    {
-      entry: ["src/index.ts", "src/adapters/elysia.ts"],
-      deps: {
-        neverBundle: ["elysia"],
-      },
+  mergeConfig(base(inlineConfig, context), {
+    entry: ["src/index.ts", "src/adapters/elysia.ts"],
+    deps: {
+      neverBundle: ["elysia"],
     },
-    base(inlineConfig, context),
-  ),
+  }),
 );
